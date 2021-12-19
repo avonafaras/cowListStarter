7e87a743674602b54c32d9e56b8524ae4f1a3b88
Anna <annasarafanova91@gmail.com>
2021-12-19 00:05:07 -0800

    Implemented Mongo DB, GET/POST work on backend

--- database/mongo/index.js
+++ database/mongo/index.js
@@ -7,13 +7,30 @@ mongoose.connect('mongodb://localhost:27017/cowList', (err)=>{
   }
   console.log('connected to mongo')
 });
+//unique!!!!!!!!
+var currentId = 0;
 
 const cowSchema = new mongoose.Schema({
-  /*
-    Fill me in
-  */
+  id: {type: Number, unique: true},
+  name: String,
+  description: String
 })
 
 const Cow = mongoose.model('Cow', cowSchema);
 
-module.exports = Cow;
+let save = (cowData) => {
+  currentId = currentId+ 1;
+  let newCow = new Cow({
+    id: currentId,
+    name: cowData.name,
+    description: cowData.description
+  })
+  return newCow.save()
+}
+
+let getAll = () => {
+  return Cow.find({}).exec();
+}
+
+module.exports.save = save ;
+module.exports.getAll = getAll;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+let db = require('../database/mongo');
+
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 })
-let db;
 
 const path = require('path');
 
@@ -11,23 +12,41 @@ const PORT = 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+app.post('/api/cows', (req, res) => {
+  db.save(req.body)
+  .then((createdCow) => {
+    res.status(201).send(createdCow)
+    console.log('Post responded with new object')
+  })
+  .catch((err) => res.status(500).send(err))
+
 })
+
+app.get('/api/cows', (req, res) => {
+  db.getAll()
+  .then((allCows) => {
+    res.status(200).send(allCows)
+  })
+  .catch((err) => res.status(500).send(err));
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening at localhost:${3000}!`);
-    readline.question(`Choose your db: (mongo or mysql)\n>>>>>`, choice=>{
-      if(choice==='mongo') {
-        console.log('Your db is Mongo');
-        db = require('../database/mongo');
-      } else if(choice === 'mysql') {
-        console.log('Your db is mysql');
-        db = require('../database/mysql');
-      } else {
-        console.log('Stope node, restart and try again, valid options are mysql and mongo')
-      }
-    })
+  // db = require('../database/mongo');
+    // readline.question(`Choose your db: (mongo or mysql)\n>>>>>`, choice=>{
+    //   if(choice==='mongo') {
+    //     console.log('Your db is Mongo');
+    //     db = require('../database/mongo');
+    //   } else if(choice === 'mysql') {
+    //     console.log('Your db is mysql');
+    //     db = require('../database/mysql');
+    //   } else {
+    //     console.log('Stope node, restart and try again, valid options are mysql and mongo')
+    //   }
+    // })
 
 });
